54666a5a5a16cf186de8370a83c1a326d1c82886
ahmadliaydin <aydin.ahmadli@merck.com>
2020-10-22 20:40:30 +0200

    adding X.info

--- thyroid_competition.py
+++ thyroid_competition.py
@@ -11,10 +11,10 @@ import lzma
 import os
 import pickle
 import urllib.request
-
 import numpy as np
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import seaborn as sn
 
 class Dataset:
     """Thyroid Dataset.
@@ -63,8 +63,11 @@ def main(args):
         'Explanatory Data Analysis'
 
         # first 15 features are Binary, remaining 6 features are Real-valued
-        # for line in range(min(X.shape[0], 5)):
+        #for line in range(min(X.shape[0], 5)):
         #    print(" ".join("{:.4g}".format(X.loc[line, column]) for column in range(min(X.shape[1], 60))))
+
+        #print(X.info()) # No NULL values.
+
 
         # TODO: Train a model on the given dataset and store it in `model`.
         model = None
