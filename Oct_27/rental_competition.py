2d1180e273ab81b42d1fdb5b3264dbe1ce1351fc
ahmadliaydin <aydin.ahmadli@merck.com>
2020-10-26 21:15:23 +0100

    deleting unnecessary comments

--- Oct_27/rental_competition.py
+++ Oct_27/rental_competition.py
@@ -60,22 +60,7 @@ def main(args):
         col_names = ['season', 'year', 'month', 'hour', 'holiday', 'day_week', 'work_day',
                      'weather', 'temp', 'feel_temp', 'humidity', 'windspeed']
         X.columns = col_names
-        """Rental Dataset.
-        The dataset instances consist of the following 12 features:
-        - [0] season (1: winter, 2: sprint, 3: summer, 4: autumn)                                  DROP
-        - [1] year (0: 2011, 1: 2012)                                                              [LEAVE-AS]
-        - [2] month (1-12)                                                                         [leave]          -normalize                                                                         
-        - [3] hour (0-23)                                                                          [ordinal]        -normalize
-        - [4] holiday (binary indicator)                                                           [LEAVE-AS]           
-        - [5] day of week (0: Sun, 1: Mon, ..., 6: Sat)                                            [ordinal]        -normalize
-        - [6] working day (binary indicator; a day is neither weekend nor holiday)                 [LEAVE-AS]
-        - [7] weather (1: clear, 2: mist, 3: light rain, 4: heavy rain)                            [leave]          -normalize
-        - [8] temperature (normalized so that -8 Celsius is 0 and 39 Celsius is 1)                 [LEAVE-AS]
-        - [9] feeling temperature (normalized so that -16 Celsius is 0 and 50 Celsius is 1)        DROP
-        - [10] relative humidity (0-1 range)                                                       [LEAVE-AS]
-        - [11] windspeed (normalized to 0-1 range)                                                 [LEAVE-AS]
-        The target variable is the number of rentals in the given hour.
-        """
+
         # ==================================================================================================
         ''' Notes from HEATMAP
         [8] and [9] have 0.99 correlation, remove one of them. They are both Celcius Temperature column 
