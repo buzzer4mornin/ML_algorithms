cd82de4d9f2c63f579d687789269889089159553
ahmadliaydin <aydin.ahmadli@merck.com>
2020-12-05 14:17:00 +0400

    update svm_multiclass.py

--- Dec_01/svm_multiclass.py
+++ Dec_01/svm_multiclass.py
@@ -27,9 +27,8 @@ def kernel(args, x, y):
     return [(args.kernel_gamma * (np.dot(x, y)) + 1) ** args.kernel_degree if args.kernel == "poly"
             else np.exp(-1 * args.kernel_gamma * ((x - y) @ (x - y)))]
 
-def smo(args, train_data, train_target, test_data, test_target,vote1,vote2, x_votes):
+def smo(args, train_data, train_target, test_data, test_target):
     # Create initial weights
-    #print("Training classes {} and {}".format(vote1, vote2))
 
     def predict_train(row):
         sums = 0
@@ -43,18 +42,11 @@ def smo(args, train_data, train_target, test_data, test_target,vote1,vote2, x_vo
             sums += a[i] * train_target[i] * test_kernels[row, i]
         return sums + b
 
-    def predict_votes(row):
-        sums = 0
-        for i in range(len(train_data)):
-            sums += a[i] * train_target[i] * test_kernels_votes[row, i]
-        return sums + b
-
     a, b = np.zeros(len(train_data)), 0
     generator = np.random.RandomState(args.seed)
 
     train_kernels = np.empty((len(train_data), len(train_data)), dtype=float)
     test_kernels = np.empty((len(test_data), len(train_data)), dtype=float)
-    test_kernels_votes = np.empty((len(x_votes), len(train_data)), dtype=float)
 
     for x1, x1_ in enumerate(train_data):
         for x2, x2_ in enumerate(train_data):
@@ -63,10 +55,6 @@ def smo(args, train_data, train_target, test_data, test_target,vote1,vote2, x_vo
     for x1, x1_ in enumerate(test_data):
         for x2, x2_ in enumerate(train_data):
             test_kernels[x1][x2] = kernel(args, x1_, x2_)[0]
-
-    for x1, x1_ in enumerate(x_votes):
-        for x2, x2_ in enumerate(train_data):
-            test_kernels_votes[x1][x2] = kernel(args, x1_, x2_)[0]
 
     passes_without_as_changing = 0
     train_accs, test_accs = [], []
@@ -118,53 +106,29 @@ def smo(args, train_data, train_target, test_data, test_target,vote1,vote2, x_vo
                         b = new_b
                         as_changed += 1
 
-        # TODO: After each iteration, measure the accuracy for both the
-        # train set and the test set and append it to `train_accs` and `test_accs`.
-        o = []
-        for i in range(len(train_data)):
-            my_predict = predict_train(i)
-            if my_predict >= 0:
-                o.append(1)
-            else:
-                o.append(-1)
-        my = sklearn.metrics.accuracy_score(o, train_target)
-
-        t = []
-        for i in range(len(test_data)):
-            my_predict_t = predict_test(i)
-            if my_predict_t >= 0:
-                t.append(1)
-            else:
-                t.append(-1)
-        my_t = sklearn.metrics.accuracy_score(t, test_target)
-
-        # TODO: VOTES VOTES VOTES VOTES VOTES VOTES
-        votes = []
-        for i in range(len(x_votes)):
-            my_predict_votes = predict_votes(i)
-            if my_predict_votes >= 0:
-                votes.append(vote1)
-            else:
-                votes.append(vote2)
-
-
-        train_accs.append(my)
-        test_accs.append(my_t)
+        train_accs, test_accs = None, None
 
         # Stop training if max_passes_without_as_changing passes were reached
         passes_without_as_changing = 0 if as_changed else passes_without_as_changing + 1
         if passes_without_as_changing >= args.max_passes_without_as_changing:
             break
 
-        # PRINTS
-        '''if len(train_accs) % 100 == 0 and len(train_accs) < args.max_iterations:
-            print("Iteration {}, train acc {:.1f}%, test acc {:.1f}%".format(
-                len(train_accs), 100 * train_accs[-1], 100 * test_accs[-1]))
 
-    print("Training finished after iteration {}, train acc {:.1f}%, test acc {:.1f}%".format(
-        len(train_accs), 100 * train_accs[-1], 100 * test_accs[-1]))'''
+    # TODO: Create an array of support vectors (in the same order in which they appeared
+    # in the training data; to avoid rounding errors, consider a training example
+    # a support vector only if a_i > `args.tolerance`) and their weights (a_i * t_i).
+    # Note that until now the full `a` should have been for prediction.
 
-    return np.array(votes).reshape(-1,1)
+    support_vectors = []
+    support_vector_weights = []
+
+    for i in range(len(a)):
+        if a[i] > args.tolerance:
+            support_vectors.append(train_data[i])
+            support_vector_weights.append((a[i]*train_target[i]))
+
+    return support_vectors, support_vector_weights, b, train_accs, test_accs
+
 
 def main(args):
     # Use the digits dataset.
@@ -222,21 +186,18 @@ def main(args):
             c8_t.append(test_data[i])
         else:
             c9_t.append(test_data[i])
-    '''c0, c1, c2, c3, c4, c5, c6, c7, c8, c9 = np.array(c0), np.array(c1), np.array(c2), np.array(c3), np.array(c4), np.array(c5), np.array(c6), np.array(c7), np.array(c8), np.array(c9)
-    c0_, c1_, c2_, c3_, c4_, c5_, c6_, c7_, c8_, c9_ = np.full(len(c0), 0), np.full(len(c1), 1), np.full(len(c2), 2), np.full(len(c3), 3), \
-                        np.full(len(c4), 4), np.full(len(c5), 5), np.full(len(c6), 6), np.full(len(c7), 7), \
-                        np.full(len(c8), 8),np.full(len(c9), 9)'''
-    '''i = 2
-    j = 5
-    res_x = np.vstack([xs[i], xs[j]])
-    res_y = np.concatenate([ys[i], ys[j]])
-    res_y = [1 if res_y[m]==2 else -1 for m in range(len(res_y))]'''
+
     xs = [np.array(c0), np.array(c1), np.array(c2), np.array(c3), np.array(c4), np.array(c5), np.array(c6), np.array(c7), np.array(c8), np.array(c9)]
     ys = [np.full(len(c0), 0), np.full(len(c1), 1), np.full(len(c2), 2), np.full(len(c3), 3), np.full(len(c4), 4), np.full(len(c5), 5), np.full(len(c6), 6), np.full(len(c7), 7), np.full(len(c8), 8),np.full(len(c9), 9)]
 
     xs_t = [np.array(c0_t), np.array(c1_t), np.array(c2_t), np.array(c3_t), np.array(c4_t), np.array(c5_t), np.array(c6_t), np.array(c7_t), np.array(c8_t), np.array(c9_t)]
     ys_t = [np.full(len(c0_t), 0), np.full(len(c1_t), 1), np.full(len(c2_t), 2), np.full(len(c3_t), 3), np.full(len(c4_t), 4), np.full(len(c5_t), 5), np.full(len(c6_t), 6), np.full(len(c7_t), 7), np.full(len(c8_t), 8), np.full(len(c9_t), 9)]
-
+    '''i = 1
+    j = 2
+    print(train_data)
+    print(train_target)
+    print(len(train_data[(train_target == i) | (train_target == j)]))
+    print(len(train_target[(train_target == i) | (train_target == j)]))'''
 
     empty = np.empty((len(test_target),1))
     result = np.empty((len(test_target),1))
@@ -248,30 +209,51 @@ def main(args):
                 continue
             d[i][j] = dict()
 
-            res_x = np.vstack([xs[i], xs[j]])
-            res_y = np.concatenate([ys[i], ys[j]])
-            res_y = [1 if res_y[m] == i else -1 for m in range(len(res_y))]
+            #TODO: problem here solved
+            #res_x = np.vstack([xs[i], xs[j]]) #res_x = train_data[(train_target == i) | (train_target == j)]       #res_x = np.vstack([xs[i], xs[j]])
+            copy_res_x = train_data[(train_target == i) | (train_target == j)]
 
-            res_x_t = np.vstack([xs_t[i], xs_t[j]])
-            res_y_t = np.concatenate([ys_t[i], ys_t[j]])
-            res_y_t = [1 if res_y_t[m] == i else -1 for m in range(len(res_y_t))]
+            #res_y = np.concatenate([ys[i], ys[j]]) #res_y = train_target[(train_target == i) | (train_target == j)]     #res_y = np.concatenate([ys[i], ys[j]])
+            #res_y = [1 if res_y[m] == i else -1 for m in range(len(res_y))]
+            copy_res_y = train_target[(train_target == i) | (train_target == j)]
+            copy_res_y = [1 if copy_res_y[m] == i else -1 for m in range(len(copy_res_y))]
 
-            arr = []
-            for u in range(args.classes):
-                arr.append(xs_t[u])
+            #res_x_t = train_data[(train_target == i) | (train_target == j)]
+            #res_y_t = train_target[(train_target == i) | (train_target == j)]
+            #res_y_t = [1 if res_y_t[m] == i else -1 for m in range(len(res_y_t))]
 
-            # TODO: wrong x_votes, it should be unordered
-            x_votes = np.vstack(arr)
-            x_votes = test_data     #TODO: SO WE CHANGE IT
-            votes = smo(args, res_x, res_y, res_x_t, res_y_t, i, j, x_votes)
-            #print("----------------{}---------------".format(len(votes)))
-            #print(votes)
+
+            #TODO: =================================== New Prediction ===================================
+            x_votes = test_data
+
+            s_vectors, s_weights, b, _, _ = smo(args, copy_res_x, copy_res_y, copy_res_x, copy_res_y)
+
+            def predict_votes(row):
+                sums = 0
+                for oo in range(len(s_weights)):
+                    sums += s_weights[oo] * test_kernels_votes[row, oo]
+                return sums + b
+
+            test_kernels_votes = np.empty((len(x_votes), len(s_vectors)), dtype=float)
+
+            for x1, x1_ in enumerate(x_votes):
+                for x2, x2_ in enumerate(s_vectors):
+                    test_kernels_votes[x1][x2] = kernel(args, x1_, x2_)[0]
+
+            votes = []
+            for t in range(len(x_votes)):
+                my_predict_votes = predict_votes(t)
+                if my_predict_votes >= 0:
+                    votes.append(i)
+                else:
+                    votes.append(j)
+
+            votes = np.array(votes).reshape(-1, 1)
+
             if i==0 and j==1:
                 result = np.c_[empty, votes]
             else:
                 result = np.c_[result, votes]
-            #print(result.shape)
-            #print(result)
 
 
     my_test = []
@@ -282,14 +264,6 @@ def main(args):
         my_test.append(int(select[0]))
 
     my_test = np.array(my_test)
-
-
-    '''import collections
-    k = collections.Counter(my_test)
-    print(k)
-    k = collections.Counter(test_target)
-    print(k)'''
-
     test_accuracy = sklearn.metrics.accuracy_score(my_test, test_target)
 
     return test_accuracy
