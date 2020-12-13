f695014f8985d3e33d59722dd6a955ffdda1db71
ahmadliaydin <aydin.ahmadli@merck.com>
2020-12-13 16:46:13 +0400

    update random_forest.py.py

--- Dec_15/random_forest.py
+++ Dec_15/random_forest.py
@@ -15,4 +15,4 @@ parser.add_argument("--recodex", default=False, action="store_true", help="Runni
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
 parser.add_argument("--test_size", default=42, type=lambda x:int(x) if x.isdigit() else float(x), help="Test set size")
 parser.add_argument("--trees", default=1, type=int, help="Number of trees in the forest")
-
+# If you add more arguments, ReCodEx will keep them with your default values.
