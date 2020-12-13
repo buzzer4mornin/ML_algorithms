737a675c22ebd41d5304aae5143f6824ed9bdfe6
ahmadliaydin <aydin.ahmadli@merck.com>
2020-12-13 16:42:58 +0400

    update decision_tree.py

--- Dec_15/decision_tree.py
+++ Dec_15/decision_tree.py
@@ -16,3 +16,12 @@ parser.add_argument("--recodex", default=False, action="store_true", help="Runni
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
 parser.add_argument("--test_size", default=42, type=lambda x:int(x) if x.isdigit() else float(x), help="Test set size")
 # If you add more arguments, ReCodEx will keep them with your default values.
+
+def main(args):
+    # Use the wine dataset
+    data, target = sklearn.datasets.load_wine(return_X_y=True)
+
+    # Split the data randomly to train and test using `sklearn.model_selection.train_test_split`,
+    # with `test_size=args.test_size` and `random_state=args.seed`.
+    train_data, test_data, train_target, test_target = sklearn.model_selection.train_test_split(
+        data, target, test_size=args.test_size, random_state=args.seed)
