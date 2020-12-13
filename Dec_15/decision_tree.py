9a29afc2913f07962a8eb015e423e0d154c81576
ahmadliaydin <aydin.ahmadli@merck.com>
2020-12-13 16:43:25 +0400

    update decision_tree.py

--- Dec_15/decision_tree.py
+++ Dec_15/decision_tree.py
@@ -31,3 +31,10 @@ def main(args):
     # - For each node, predict the most frequent class (and the one with
     #   smallest index if there are several such classes).
     #
+    # - When splitting a node, consider the features in sequential order, then
+    #   for each feature consider all possible split points ordered in ascending
+    #   value, and perform the first encountered split descreasing the criterion
+    #   the most. Each split point is an average of two nearest unique feature values
+    #   of the instances corresponding to the given node (i.e., for four instances
+    #   with values 1, 7, 3, 3 the split points are 2 and 5).
+    #
