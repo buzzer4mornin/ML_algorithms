ab71bc080f6fe86dfda15f9f07fe02c22ab923f8
ahmadliaydin <aydin.ahmadli@merck.com>
2020-12-13 16:46:04 +0400

    update random_forest.py.py

--- Dec_15/random_forest.py
+++ Dec_15/random_forest.py
@@ -5,3 +5,14 @@ import numpy as np
 import sklearn.datasets
 import sklearn.metrics
 import sklearn.model_selection
+
+parser = argparse.ArgumentParser()
+# These arguments will be set appropriately by ReCodEx, even if you change them.
+parser.add_argument("--bootstrapping", default=False, action="store_true", help="Perform data bootstrapping")
+parser.add_argument("--feature_subsampling", default=1, type=float, help="What fraction of features to subsample")
+parser.add_argument("--max_depth", default=None, type=int, help="Maximum decision tree depth")
+parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
+parser.add_argument("--seed", default=42, type=int, help="Random seed")
+parser.add_argument("--test_size", default=42, type=lambda x:int(x) if x.isdigit() else float(x), help="Test set size")
+parser.add_argument("--trees", default=1, type=int, help="Number of trees in the forest")
+
