1ed11f0ac05308dda5dcb5e9094675fb43f9f0b3
ahmadliaydin <aydin.ahmadli@merck.com>
2021-01-03 15:07:17 +0400

    adding files

--- Jan_05/gradient_boosting.py
+++ Jan_05/gradient_boosting.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 import argparse
+# coding:utf-8
+import numpy as np
+# logistic function
+from scipy.special import expit
 
+from .base import BaseEstimator
+from .tree import Tree
 import numpy as np
 import sklearn.datasets
 import sklearn.metrics
@@ -29,33 +35,123 @@ def main(args):
 
     classes = np.max(target) + 1
 
+    def mse_criterion(y, splits):
+        y_mean = np.mean(y)
+        return -sum([np.sum((split - y_mean) ** 2) * (float(split.shape[0]) / y.shape[0]) for split in splits])
 
-    # TODO: Create a gradient boosted trees on the classification training data.
-    #
-    # Notably, train for `args.trees` iteration. During iteration `t`:
-    # - the goal is to train `classes` regression trees, each predicting
-    #   raw weight for the corresponding class.
-    # - compute the current predictions `y_t(x_i)` for every training example `i` as
-    #     y_t(x_i)_c = \sum_{i=1}^t args.learning_rate * tree_{iter=i,class=c}.predict(x_i)
-    #     (note that y_0 is zero)
-    # - loss in iteration `t` is
-    #     L = (\sum_i NLL(target_i, softmax(y_{t-1}(x_i) + trees_to_train_in_iter_t.predict(x_i)))) +
-    #         1/2 * args.l2 * (sum of all node values in trees_to_train_in_iter_t)
-    # - for every class `c`:
-    #   - start by computing `g_i` and `h_i` for every training example `i`;
-    #     the `g_i` is the first derivative of NLL(target_i_c, softmax(y_{t-1}(x_i))_c)
-    #     with respect to y_{t-1}(x_i)_c, and the `h_i` is the second derivative of the same.
-    #   - then, create a decision tree minimizing the above loss L. According to the slides,
-    #     the optimum prediction for a given node T with training examples I_T is
-    #       w_T = - (\sum_{i \in I_T} g_i) / (args.l2 + sum_{i \in I_T} h_i)
-    #     and the value of the loss with the above prediction is
-    #       c_GB = - 1/2 (\sum_{i \in I_T} g_i)^2 / (args.l2 + sum_{i \in I_T} h_i)
-    #     which you should use as a splitting criterion.
-    #
-    # During tree construction, we split a node if:
-    # - its depth is less than `args.max_depth`
-    # - there is more than 1 example corresponding to it (this was covered by
-    #     a non-zero criterion value in the previous assignments)
+    """
+    References:
+    https://arxiv.org/pdf/1603.02754v3.pdf
+    http://www.saedsayad.com/docs/xgboost.pdf
+    https://homes.cs.washington.edu/~tqchen/pdf/BoostedTree.pdf
+    http://stats.stackexchange.com/questions/202858/loss-function-approximation-with-taylor-expansion
+    """
+
+    class Loss:
+        """Base class for loss functions."""
+
+        def __init__(self, regularization=1.0):
+            self.regularization = regularization
+
+        def grad(self, actual, predicted):
+            """First order gradient."""
+            raise NotImplementedError()
+
+        def hess(self, actual, predicted):
+            """Second order gradient."""
+            raise NotImplementedError()
+
+        def approximate(self, actual, predicted):
+            """Approximate leaf value."""
+            return self.grad(actual, predicted).sum() / (self.hess(actual, predicted).sum() + self.regularization)
+
+        def transform(self, pred):
+            """Transform predictions values."""
+            return pred
+
+        def gain(self, actual, predicted):
+            """Calculate gain for split search."""
+            nominator = self.grad(actual, predicted).sum() ** 2
+            denominator = self.hess(actual, predicted).sum() + self.regularization
+            return 0.5 * (nominator / denominator)
+
+    class LogisticLoss(Loss):
+        """Logistic loss."""
+
+        def grad(self, actual, predicted):
+            return actual * expit(-actual * predicted)
+
+        def hess(self, actual, predicted):
+            expits = expit(predicted)
+            return expits * (1 - expits)
+
+        def transform(self, output):
+            # Apply logistic (sigmoid) function to the output
+            return expit(output)
+
+    class GradientBoosting(BaseEstimator):
+        """Gradient boosting trees with Taylor's expansion approximation (as in xgboost)."""
+
+        def __init__(self, n_estimators, learning_rate=0.1, max_features=10, max_depth=2, min_samples_split=10):
+            self.min_samples_split = min_samples_split
+            self.learning_rate = learning_rate
+            self.max_depth = max_depth
+            self.max_features = max_features
+            self.n_estimators = n_estimators
+            self.trees = []
+            self.loss = None
+
+        def fit(self, X, y=None):
+            self._setup_input(X, y)
+            self.y_mean = np.mean(y)
+            self._train()
+
+        def _train(self):
+            # Initialize model with zeros
+            y_pred = np.zeros(self.n_samples, np.float32)
+
+            for n in range(self.n_estimators):
+                residuals = self.loss.grad(self.y, y_pred)
+                tree = Tree(regression=True, criterion=mse_criterion)
+                # Pass multiple target values to the tree learner
+                targets = {
+                    # Residual values
+                    "y": residuals,
+                    # Actual target values
+                    "actual": self.y,
+                    # Predictions from previous step
+                    "y_pred": y_pred,
+                }
+                tree.train(
+                    self.X,
+                    targets,
+                    max_features=self.max_features,
+                    min_samples_split=self.min_samples_split,
+                    max_depth=self.max_depth,
+                    loss=self.loss,
+                )
+                predictions = tree.predict(self.X)
+                y_pred += self.learning_rate * predictions
+                self.trees.append(tree)
+
+        def _predict(self, X=None):
+            y_pred = np.zeros(X.shape[0], np.float32)
+
+            for i, tree in enumerate(self.trees):
+                y_pred += self.learning_rate * tree.predict(X)
+            return y_pred
+
+        def predict(self, X=None):
+            return self.loss.transform(self._predict(X))
+
+    class GradientBoostingClassifier(GradientBoosting):
+        def fit(self, X, y=None):
+            # Convert labels from {0, 1} to {-1, 1}
+            y = (y * 2) - 1
+            self.loss = LogisticLoss()
+            super(GradientBoostingClassifier, self).fit(X, y)
+
+
 
     # TODO: Finally, measure your training and testing accuracies when
     # using 1, 2, ..., `args.trees` of the created trees.
