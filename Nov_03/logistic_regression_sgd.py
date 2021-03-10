7314d897cf727740335aaf0e75bf65a2f30c3c4d
ahmadliaydin <aydin.ahmadli@merck.com>
2021-03-10 23:07:42 +0400

    update

--- Nov_03/logistic_regression_sgd.py
+++ Nov_03/logistic_regression_sgd.py
@@ -18,7 +18,10 @@ parser.add_argument("--learning_rate", default=0.01, type=float, help="Learning 
 parser.add_argument("--plot", default=False, const=True, nargs="?", type=str, help="Plot the predictions")
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
-parser.add_argument("--test_size", default=0.5, type=lambda x:int(x) if x.isdigit() else float(x), help="Test set size")
+parser.add_argument("--test_size", default=0.5, type=lambda x: int(x) if x.isdigit() else float(x),
+                    help="Test set size")
+
+
 # If you add more arguments, ReCodEx will keep them with your default values.
 
 def main(args):
@@ -26,13 +29,10 @@ def main(args):
     generator = np.random.RandomState(args.seed)
 
     # Generate an artifical regression dataset
-    data, target = sklearn.datasets.make_classification(
-        n_samples=args.data_size, n_features=2, n_informative=2, n_redundant=0, random_state=args.seed)
-
+    data, target = sklearn.datasets.make_classification(n_samples=args.data_size, n_features=2, n_informative=2, n_redundant=0, random_state=args.seed)
 
     # TODO: Append a constant feature with value 1 to the end of every input data
     data = np.pad(data, ((0, 0), (0, 1)), constant_values=1)
-
 
     # TODO: Split the dataset into a train set and a test set.
     # Use `sklearn.model_selection.train_test_split` method call, passing
@@ -41,11 +41,11 @@ def main(args):
         sklearn.model_selection.train_test_split(
             data, target,
             test_size=args.test_size, random_state=args.seed
-            )
         )
+    )
 
     # Generate initial linear regression weights
-    #weights = generator.uniform(size=train_data.shape[1])
+    # weights = generator.uniform(size=train_data.shape[1])
     weights = np.zeros(train_data.shape[1])
 
     for iteration in range(args.iterations):
@@ -56,9 +56,8 @@ def main(args):
         # You can assume that `args.batch_size` exactly divides `train_data.shape[0]`.
         gradient, gradient_components = 0, 0
         for i in permutation:
-            gradient += (sigmoid(np.dot(train_data[i], weights)) - train_target[i]) * train_data[i] # (train_data[i] @ weights - train_target[i]) * train_data[i]
+            gradient += (sigmoid(np.dot(train_data[i], weights)) - train_target[i]) * train_data[i]  # (train_data[i] @ weights - train_target[i]) * train_data[i]
             gradient_components += 1
-            print(weights)
             if gradient_components == args.batch_size:
                 weights -= args.learning_rate * gradient / gradient_components
                 gradient, gradient_components = 0, 0
@@ -68,15 +67,15 @@ def main(args):
         # train test and the test set. The loss is the average MLE loss (i.e., the
         # negative log likelihood, or crossentropy loss, or KL loss) per example.
 
-        train_pred_p    = sigmoid(train_data @ weights)
-        train_pred      = train_pred_p >= 0.5
-        train_accuracy  = np.sum(train_pred==train_target)/train_data.shape[0]
-        train_loss      = sklearn.metrics.log_loss(train_target, train_pred_p)
+        train_pred_p = sigmoid(train_data @ weights)
+        train_pred = train_pred_p >= 0.5
+        train_accuracy = np.sum(train_pred == train_target) / train_data.shape[0]
+        train_loss = sklearn.metrics.log_loss(train_target, train_pred_p)
 
-        test_pred_p     = sigmoid(test_data @ weights)
-        test_pred       = test_pred_p >= 0.5
-        test_accuracy   = np.sum(test_pred==test_target)/test_data.shape[0]
-        test_loss       = sklearn.metrics.log_loss(test_target, test_pred_p)
+        test_pred_p = sigmoid(test_data @ weights)
+        test_pred = test_pred_p >= 0.5
+        test_accuracy = np.sum(test_pred == test_target) / test_data.shape[0]
+        test_loss = sklearn.metrics.log_loss(test_target, test_pred_p)
 
         print("After iteration {}: train loss {:.4f} acc {:.1f}%, test loss {:.4f} acc {:.1f}%".format(
             iteration + 1, train_loss, 100 * train_accuracy, test_loss, 100 * test_accuracy))
@@ -84,8 +83,8 @@ def main(args):
         if args.plot:
             import matplotlib.pyplot as plt
             if args.plot is not True:
-                if not iteration: plt.figure(figsize=(6.4*3, 4.8*(args.iterations+2)//3))
-                plt.subplot(3, (args.iterations+2)//3, 1 + iteration)
+                if not iteration: plt.figure(figsize=(6.4 * 3, 4.8 * (args.iterations + 2) // 3))
+                plt.subplot(3, (args.iterations + 2) // 3, 1 + iteration)
             xs = np.linspace(np.min(data[:, 0]), np.max(data[:, 0]), 50)
             ys = np.linspace(np.min(data[:, 1]), np.max(data[:, 1]), 50)
             predictions = [[1 / (1 + np.exp(-([x, y, 1] @ weights))) for x in xs] for y in ys]
@@ -94,10 +93,13 @@ def main(args):
             plt.scatter(train_data[:, 0], train_data[:, 1], c=train_target, marker="P", label="train", cmap=plt.cm.RdBu)
             plt.scatter(test_data[:, 0], test_data[:, 1], c=test_target, label="test", cmap=plt.cm.RdBu)
             plt.legend(loc="upper right")
-            if args.plot is True: plt.show()
-            else: plt.savefig(args.plot, transparent=True, bbox_inches="tight")
+            if args.plot is True:
+                plt.show()
+            else:
+                plt.savefig(args.plot, transparent=True, bbox_inches="tight")
 
     return weights
+
 
 if __name__ == "__main__":
     args = parser.parse_args([] if "__file__" not in globals() else None)
--- Oct_27/train.py
+++
@@ -1,7 +0,0 @@
-a = 5
-
-def foo (x):
-    print(dir())
-    return x**2
-
-foo(5)
